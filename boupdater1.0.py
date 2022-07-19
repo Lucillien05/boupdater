13e22bde05dad7b159842728575703ac76865012
Lucillien05 <109629766+Lucillien05@users.noreply.github.com>
2022-07-19 23:31:37 +0200

    Add files via upload

---
+++ boupdater1.0.py
@@ -0,0 +1,28 @@
+import urllib.request
+import os
+import sys
+import wget
+import shutil
+os.chdir(os.path.dirname(os.path.dirname(sys.argv[0])))
+patches = ['H','P','S','T']
+mpqs = ['mpq','MPQ','Mpq']
+patchs = ['patch', 'PATCH', 'Patch']
+for patch in patches:
+    for mpq in mpqs:
+        for patchurl in patchs:
+            try:
+                print(f'Try {patchurl}-{patch}.{mpq}')
+                if os.path.exists(f'Data\{patchurl}-{patch}.{mpq}'):
+                    if int(urllib.request.urlopen(f'https://bradavice-online.cz/patches/{patchurl}-{patch}.{mpq}').length) != int(os.path.getsize(f'Data/{patchurl}-{patch}.{mpq}')):
+                        os.remove(f'Data\{patchurl}-{patch}.{mpq}')
+                        wget.download(f'https://bradavice-online.cz/patches/{patchurl}-{patch}.{mpq}', f'Data\{patchurl}-{patch}.{mpq}')
+                        print(f'Update {patchurl}-{patch}.{mpq}')
+                else:
+                    print(f'Download {patchurl}-{patch}.{mpq}')
+                    wget.download(f'https://bradavice-online.cz/patches/{patchurl}-{patch}.{mpq}', f'Data\{patchurl}-{patch}.{mpq}')
+            except:
+                print(f'Except {patchurl}-{patch}.{mpq}')
+if os.path.exists('Cache'):
+    shutil.rmtree('Cache')
+os.startfile('bradavice.exe')
+sys.exit()
