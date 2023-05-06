f21d02625640047b1600829b268e2da845b99112
Lucillien05 <109629766+Lucillien05@users.noreply.github.com>
2023-05-06 13:04:24 +0200

    Add files via upload

--- boupdater1.0.py
+++ boupdater1.0.py
@@ -3,9 +3,11 @@ import os
 import sys
 import wget
 import shutil
+import time
 os.chdir(os.path.dirname(os.path.dirname(sys.argv[0])))
 fail = 0
 noupt = 0
+done = 0
 patches = ['H','P','S','T']
 mpqs = ['mpq','MPQ','Mpq']
 patchs = ['patch', 'PATCH', 'Patch']
@@ -18,6 +20,7 @@ for patch in patches:
                         print(f'\nPatch-{patch} is outdated. Updating...')
                         os.remove(f'Data\{patchurl}-{patch}.{mpq}')
                         wget.download(f'https://bradavice-online.cz/patches/{patchurl}-{patch}.{mpq}', f'Data\{patchurl}-{patch}.{mpq}')
+                        done += 1
                     elif int(urllib.request.urlopen(f'https://bradavice-online.cz/patches/{patchurl}-{patch}.{mpq}').length) == int(os.path.getsize(f'Data/{patchurl}-{patch}.{mpq}')):
                         noupt += 1
                 else:
@@ -25,13 +28,16 @@ for patch in patches:
                         print('Error')
                     print(f'\nNo Patch-{patch} detected. Downloading new...')
                     wget.download(f'https://bradavice-online.cz/patches/{patchurl}-{patch}.{mpq}', f'Data\{patchurl}-{patch}.{mpq}')
+                    done += 1
             except:
                 fail += 1
+if done != 0:
+    print('\n')
 if os.path.exists('Data\Patch-M.mpq') and int(os.path.getsize('Data/Patch-M.mpq')) == 8707764965:
     if noupt == 4:
         print('All patches are already updated.')
 else:
-    print('\nPatch-M is not updated or installed properly.')
+    print('Patch-M is not updated or installed properly.')
     m = input('Proceed? (y/n): ')
     if m == 'y' or m == '1':
         pass
@@ -39,6 +45,7 @@ else:
         sys.exit()
 if os.path.exists('Cache'):
     shutil.rmtree('Cache')
-print('\nOpening Bradavice-Online:')
+    print('Deleting Cache.')
+print('Opening Bradavice-Online:')
 os.startfile('bradavice.exe')
 sys.exit()
